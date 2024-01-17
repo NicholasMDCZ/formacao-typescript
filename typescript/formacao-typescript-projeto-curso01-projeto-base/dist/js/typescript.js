8a63861442b534621c555435dc3806e0471902d8
Nicholas Castro <contabrpt2@hotmail.com>
2024-01-17 17:09:32 -0300

    Add files via upload

--- typescript/formacao-typescript-projeto-curso01-projeto-base/dist/js/typescript.js
+++ typescript/formacao-typescript-projeto-curso01-projeto-base/dist/js/typescript.js
@@ -1,1 +1,12 @@
 let valor = 3000;
+let nome = "";
+let isPago = false;
+let qualquer = "";
+qualquer = 22;
+const lista = [];
+lista.push(12, 19, 5, 66, 1.19);
+const novaTransacao = {
+    tipoTransacao: "",
+    data: new Date(),
+    valor: 0
+};
--- typescript/formacao-typescript-projeto-curso01-projeto-base/src/typescript.ts
+++ typescript/formacao-typescript-projeto-curso01-projeto-base/src/typescript.ts
@@ -1,1 +1,20 @@
 let valor: number = 3000;
+let nome: string = "";
+let isPago: boolean = false;
+let qualquer: any = "";
+qualquer = 22;
+
+const lista: number[] = [];
+lista.push(12, 19, 5, 66, 1.19)
+
+type Transacao = {
+    tipoTransacao: string;
+    data: Date;
+    valor: number;
+}
+
+const novaTransacao: Transacao = {
+    tipoTransacao: "",
+    data: new Date(),
+    valor: 0
+}
