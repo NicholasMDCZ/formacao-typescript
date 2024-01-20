719a9c4fd00bd7b63b73d81e86d76961b7545be2
Nicholas Castro <contabrpt2@hotmail.com>
2024-01-20 02:49:06 -0300

    Add files via upload

--- typescript/formacao-typescript-projeto-curso01-projeto-base/src/types/Conta.ts
+++ typescript/formacao-typescript-projeto-curso01-projeto-base/src/types/Conta.ts
@@ -2,6 +2,13 @@ import { Transacao } from "./Transacao.js";
 import { TipoTransacao } from "./TipoTransacao.js";
 
 let saldo: number = 3000;
+const transacoes: Transacao[] = JSON.parse(localStorage.getItem("transacoes"), (key: string, value: string) =>{
+    if(key === "data") {
+        return new Date(value);
+    }
+
+    return value;
+ }) || [];
 
 function debitar(valor: number): void {
     if (valor <= 0) {
@@ -10,6 +17,7 @@ function debitar(valor: number): void {
     if (valor > saldo) {
         throw new Error("Saldo insuficiente!");
     }
+
     saldo -= valor;
 }
 
@@ -17,6 +25,7 @@ function depositar(valor: number): void{
     if (valor <= 0) {
         throw new Error("O valor a ser depositado deve ser maior que zero!");
     }
+
     saldo += valor;
 }
 
@@ -39,6 +48,10 @@ const Conta = {
         else {
             throw new Error("Tipo de Transação é Inválido!");
         }
+
+        transacoes.push(novaTransacao);
+        console.log(transacoes);
+        localStorage.setItem("transacoes", JSON.stringify(transacoes));
     }
 }
 
