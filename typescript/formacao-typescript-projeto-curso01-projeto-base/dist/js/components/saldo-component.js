6bbd298c94c9d590925fa2a7541e15ec65ed87dc
Nicholas Castro <contabrpt2@hotmail.com>
2024-01-20 00:23:46 -0300

    Add files via upload

--- typescript/formacao-typescript-projeto-curso01-projeto-base/dist/js/components/saldo-component.js
+++ typescript/formacao-typescript-projeto-curso01-projeto-base/dist/js/components/saldo-component.js
@@ -1,18 +1,20 @@
 import { formatarData, formatarMoeda } from "../utils/formaters.js";
-let saldo = 3000;
+import { FormatoData } from "../types/FormatoData.js";
+import Conta from "../types/Conta.js";
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
 const elementoDataAcesso = document.querySelector(".block-saldo time");
 if (elementoDataAcesso != null) {
-    const dataAcesso = new Date();
-    elementoDataAcesso.textContent = formatarData(dataAcesso);
+    elementoDataAcesso.textContent = formatarData(Conta.getDataAcesso(), FormatoData.DIA_SEMANA_DIA_MES_ANO);
 }
-export function getSaldo() {
-    return saldo;
-}
-atualizarSaldo(saldo);
-export function atualizarSaldo(novoSaldo) {
-    saldo = novoSaldo;
+renderizarSaldo();
+function renderizarSaldo() {
     if (elementoSaldo != null) {
-        elementoSaldo.textContent = formatarMoeda(saldo);
+        elementoSaldo.textContent = formatarMoeda(Conta.getSaldo());
     }
 }
+const SaldoComponent = {
+    atualizar() {
+        renderizarSaldo();
+    }
+};
+export default SaldoComponent;
