5ffc1b351fae72c886f517863fa7a64473beb51a
Nicholas Castro <contabrpt2@hotmail.com>
2024-01-19 23:20:55 -0300

    Add files via upload

---
+++ typescript/formacao-typescript-projeto-curso01-projeto-base/dist/js/components/nova-transacao-component.js
@@ -0,0 +1,32 @@
+const elementoFormulario = document.querySelector(".block-nova-transacao form");
+elementoFormulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (!elementoFormulario.checkValidity()) {
+        alert("Por favor, Preencha todos os campos da transação!");
+        return;
+    }
+    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+    const inputValor = elementoFormulario.querySelector("#valor");
+    const inputData = elementoFormulario.querySelector("#data");
+    let tipoTransacao = inputTipoTransacao.value;
+    let valor = inputValor.valueAsNumber;
+    let data = new Date(inputData.value);
+    if (tipoTransacao == "Depósito") {
+        saldo += valor;
+    }
+    else if (tipoTransacao == TipoTransacao.TRANSFERENCIA || tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO) {
+        saldo -= valor;
+    }
+    else {
+        alert("Tipo de Transação é Inválido!");
+        return;
+    }
+    elementoSaldo.textContent = formatarMoeda(saldo);
+    const novaTransacao = {
+        tipoTransacao: tipoTransacao,
+        valor: valor,
+        data: data
+    };
+    console.log(novaTransacao);
+    elementoFormulario.reset();
+});
